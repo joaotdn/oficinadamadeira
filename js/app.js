9c1d4bba7974418a890515d8eb51af975f27d88d
Joao Teodoro <joaotdn@gmail.com>
2014-05-07 02:13:05 -0300

    Update home

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+/**
+ * Configure preload site
+ */
+$(document).ready(function() {
+    $('body').jpreLoader({
+      showPercentage: false,
+      loaderVPos: '400px',
+      //autoClose: false,
+      splashVPos: '202px',
+      closeBtnText: ''
+    });
+}, new WOW().init());
+
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
